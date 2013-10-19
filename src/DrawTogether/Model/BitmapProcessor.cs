f7c30c99631bcb82363d8dc810329ed29e9d307f
Ivan Manov <ivanmanov@live.com>
2013-10-19 13:59:29 +0400

    ResultsProvider, BitmapProcessor заготовки (не додел.)

---
+++ src/DrawTogether/Model/BitmapProcessor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace DrawTogether.Model
+{
+    public class BitmapProcessor
+    {
+        public BitmapProcessor()
+        { 
+        }
+
+        public double GetSimilarityValue(BitmapSource baseSource, BitmapSource resultSource)
+        {
+            return 0; //затычка
+        }
+    }
+}
---
+++ src/DrawTogether/Model/ResultsProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawTogether.Model
+{
+    public class ResultsProvider
+    {
+        private BitmapProcessor _bitmapProcessor;
+
+        public ResultsProvider(BitmapProcessor bitmapProcessor)
+        {
+            _bitmapProcessor = bitmapProcessor;
+        }
+
+        public Player[] UpdatePlayersResults(Player[] players, double rateCoeff) //inject to "out" + void
+        {
+            foreach (var player in players)
+            {
+                player.Score = rateCoeff * _bitmapProcessor.GetSimilarityValue(player.SourceBitmap, player.ResultBitmap);
+            }
+
+            return players;
+        }
+    }
+}
